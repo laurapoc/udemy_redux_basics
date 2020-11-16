3c2ec3fa44ea910a6b14dd8587d7fe3e079a7ed2
laurapoc <laura.pociuniene@gmail.com>
2020-11-16 15:15:10 +0200

    first commit

---
+++ redux-basics.js
@@ -0,0 +1,16 @@
+var redux = require("redux");
+var createStore = redux.createStore;
+
+// Reducer
+// var rootReducer = (state, action) => {
+//     return state;
+// };
+
+// Store
+var store = createStore(rootReducer);
+
+
+
+// Dispatching Action
+
+// Subscription
