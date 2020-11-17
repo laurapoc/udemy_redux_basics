0d7738a9002036dc3f71b639ff476fdb1e81507f
laurapoc <laura.pociuniene@gmail.com>
2020-11-17 15:59:56 +0200

    redux introduction

--- redux-basics.js
+++ redux-basics.js
@@ -27,11 +27,13 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 
+// Subscription
+store.subscribe(() => {
+    console.log("[subscription]", store.getState());
+});
 
 // Dispatching Action
 store.dispatch({type: "INC_COUNTER"});
 store.dispatch({type: "ADD_COUNTER", value: 10});
 console.log(store.getState());
 
-
-// Subscription
