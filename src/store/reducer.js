3ce8528278afbca07300d66cfac17052f4a98860
laurapoc <laura.pociuniene@gmail.com>
2020-11-18 11:51:09 +0200

    practice dispatching actions-instructor's version

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,30 @@
 const initialState = {
-    counter: 0
-}
+  counter: 0,
+};
 
 const reducer = (state = initialState, action) => {
-    if(action.type === "INCREMENT") {
-        return {
-            counter: state.counter + 1
-        }
-    }
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + 10,
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - 8,
+    };
+  }
 
-
-
-    return state;
+  return state;
 };
 
 export default reducer;
