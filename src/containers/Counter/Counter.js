dbfd35e551f9a3bbe8eb7b99c0040ff9fffdc81b
laurapoc <laura.pociuniene@gmail.com>
2020-11-18 13:22:18 +0200

    async data

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -13,6 +13,11 @@ class Counter extends Component {
         <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
         <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
         <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+            <li onClick={this.props.onDeleteResult}></li>
+        </ul>
       </div>
     );
   }
@@ -30,6 +35,8 @@ const mapDispatchToProps = (dispatch) => {
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
     onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 }),
+    onStoreResult: () => dispatch({type: "STORE_RESULT"}),
+    onDeleteResult: () => dispatch({type: "DELETE_RESULT"}),
   };
 };
 
